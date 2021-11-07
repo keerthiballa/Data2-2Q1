54661cafbcb88f38d852a8094235b30249dd9d3e
Balla Keerthi <keerthiballa@gmail.com>
2021-11-07 13:57:45 -0500

    Complete Exercise 2. Passed 1 out of 1 test

--- quiz1/fibonacci.py
+++ quiz1/fibonacci.py
@@ -1,5 +1,18 @@
-def fibonacci(fin: float) -> float:
-    """
-    build a classic fibonacci function - No RECURSION
-    """
-    pass  # implement me
+def fibonacci(n: int) -> int:
+    first_num = 0
+    sec_num = 1
+    if n>=0:
+        for i in range(1,n):
+            third_num = first_num + sec_num
+            first_num = sec_num
+            sec_num = third_num
+    return third_num
+
+#(venv) (base) ballakeerthi@zipcodes-MacBook-Pro-3 Data2-2Q1 % python3 -m unittest quiz1.test_fibonacci
+#.
+#----------------------------------------------------------------------
+#Ran 1 test in 0.000s
+
+#OK
+#(venv) (base) ballakeerthi@zipcodes-MacBook-Pro-3 Data2-2Q1 %
+#

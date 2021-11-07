2dd4134d6d93c90e7a7402c42651b2f5f6223082
Balla Keerthi <keerthiballa@gmail.com>
2021-11-07 14:13:53 -0500

    Complete Exercise 3. Passed 1 out of 1 test

--- quiz1/palindrome.py
+++ quiz1/palindrome.py
@@ -4,4 +4,21 @@ def palindrome(value: str) -> bool:
     :param value: A string
     :return: A boolean
     """
-    pass  # remove pass statement and implement me
+
+    j=len(value)-1
+    for i in range(0,j,1):
+        if value[i] != value[j]:
+            return False
+
+        j -= 1
+        return True
+
+#(venv) (base) ballakeerthi@zipcodes-MacBook-Pro-3 Data2-2Q1 % python3 -m unittest quiz1.test_palim
+#.
+#----------------------------------------------------------------------
+#Ran 1 test in 0.000s
+
+#OK
+#(venv) (base) ballakeerthi@zipcodes-MacBook-Pro-3 Data2-2Q1 %
+
+
